fbd0ea3c69e787a70504dd303506b28e884cb60e
JAHID HASSAN <75775860+jahid001-dev@users.noreply.github.com>
2023-10-19 21:34:06 +0600

    6 featured section added with server data and showing ui

---
+++ src/components/MainSection/Featured/Featured.jsx
@@ -0,0 +1,38 @@
+import { Link } from "react-router-dom";
+
+const Featured = ({ company }) => {
+    const { companyName, _id, image, brandMotto } = company;
+    return (
+        <div className="max-w-7xl  mx-auto">
+            <div >
+                <Link to={`/brandDetails/${_id}`}>
+                    <button className="group relative">
+                        <div className="group-hover:bg-opacity-80 transition-opacity duration-700">
+                            <div className="card w-80 h-80 hover:opacity-50   image-full relative">
+                                <figure>
+                                    <img
+                                        src={image}
+                                        alt={companyName}
+                                        className="transition-transform transform hover:scale-105"
+                                    />
+                                </figure>
+                                <div className="card-body">
+                                    <h2 className="text-center font-mono font-bold text-2xl">
+                                        {companyName}
+                                    </h2>
+                                </div>
+                                <div className="card-body absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-700">
+                                    <h2 className="card-title text-2xl font-mono">
+                                        {brandMotto}
+                                    </h2>
+                                </div>
+                            </div>
+                        </div>
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
+};
+
+export default Featured;
--- src/components/MainSection/MainSection.jsx
+++ src/components/MainSection/MainSection.jsx
@@ -1,10 +1,24 @@
+import { useLoaderData } from "react-router-dom";
+import Featured from "./Featured/Featured";
 import Testimonials from "./Testimonials/Testimonials";
 import WhyChooseUs from "./WhyChooseUs/WhyChooseUs";
 
 
 const MainSection = () => {
+    const companies = useLoaderData()
     return (
         <div>
+            <h2 className="text-5xl text-center my-32 font-bold font-serif">
+                Our Partners
+            </h2>
+            <div className="max-w-7xl  mx-auto">
+                <div className="grid md:ml-3 grid-cols-1 md:grid-cols-3 gap-3">
+
+                    {companies.map((company) => (
+                        <Featured key={company.id} company={company}></Featured>
+                    ))}
+                </div>
+            </div>
             <WhyChooseUs></WhyChooseUs>
             <Testimonials></Testimonials>
         </div>
